93bce3d1f44e6508f7f595a485ca0a3d92d8002a
Abdul Mubeen <abdulmubeen57@gmail.com>
2023-03-16 22:53:33 +0530

    Added Posts Controller and Route
    Fixed improper imports and added Posts Schema, Controller, and Route

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -47,7 +47,7 @@ export const login = async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
-      return res.status(400).json({ msg: "Invalid credentials..." });
+      return res.status(400).json({ message: "Invalid credentials..." });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
     delete user.password;
